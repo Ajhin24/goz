d091233aded9c187bcd8d3386f4b949ee077ab1b
Ajhin24 <108016079+Ajhin24@users.noreply.github.com>
2022-06-23 00:43:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+			<title>Goz</title>
+		<body>
+			<img src="Gozon.jpg"style="height: 200;"> </a>
+			<h1>Name</h1>
+			<p> <b>Last name: </b>Gozon <b>Given name:</b> Mark Lester <b>Middle Name:</b> Monte<b> Suffix:</b> N/A <b>Nickname:</b>Macmac <br><br> <b>Age:</b>19 <br><br><b>Birthday:</b>April 24,2002 
+			<h1>Hobbies</h1>
+			<img src="valo.jpg"style="width: 11%";>
+			<img src="lol.jpg"style="height: 20%;">
+			<img src="CF.jpg"style="height: 20%;">
+			<h1>Social Media Accounts</h1>
+			<a href="https://www.facebook.com/MarkLesterGozon"> <img src="FB.jpg" style="width:15%";></a>
+			<a href="https://www.instagram.com/?hl=en"> <img src="instagram.jpg" style="width:9%";> </a>
+			<a href="https://www.Twitter.com/@Lestwerr"> <img src="Twitter.jpg" style="width:12%";> </a>
+		</body>
+	</head>
+</html>
